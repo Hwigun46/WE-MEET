c07723f287d923f5e0cf57a7dc8a0781b3e62d24
hwigun46 <hy010827@naver.com>
2025-05-06 19:26:47 +0000

    [FIX] file_open 수정

--- src/bpf/file_open.bpf.c
+++ src/bpf/file_open.bpf.c
@@ -6,8 +6,15 @@
 // 라이센스 설정
 char LICENSE[] SEC("license") = "GPL";
 
+// map 정의
+struct
+{
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} file_open_event_map SEC(".maps");
+
+// 파일 열기(생성) Tracepoint
 SEC("tracepoint/syscalls/sys_enter_openat")
-int handle_file_open(struct trace_event_raw_sys_enter *ctx)
+static int handle_file_open(struct trace_event_raw_sys_enter *ctx)
 {
 
     struct file_open_event_t evt = {};
@@ -37,24 +44,18 @@ int handle_file_open(struct trace_event_raw_sys_enter *ctx)
     // comm
     bpf_get_current_comm(&evt.base.comm, sizeof(evt.base.comm));
 
-    // p_comm
-    bpf_core_read_str(&evt.base.parent_comm, sizeof(evt.base.parent_comm), parent->comm);
-
     // timestamp
     evt.base.timestamp_ns = bpf_ktime_get_ns();
 
     // filename
-    const char *filename_ptr = (const char *)ctx->args[1];
-    bpf_core_read_str(evt.filename, sizeof(evt.filename), filename_ptr);
+    const char *filename = (const char *)ctx->args[1];
+    if (bpf_core_read_user_str(&evt.filename, sizeof(evt.filename), filename) < 1)
+        return 0;
 
-    //flags
     evt.flags = (int)ctx->args[2];
-
-    // mode
     evt.mode = (int)ctx->args[3];
 
-    // event map에 보내기
-    bpf_perf_event_output(ctx, &event_output_map, BPF_F_CURRENT_CPU, &evt, sizeof(evt));
+    bpf_perf_event_output(ctx, &file_open_event_map, BPF_F_CURRENT_CPU, &evt, sizeof(evt));
 
     return 0;
 }
