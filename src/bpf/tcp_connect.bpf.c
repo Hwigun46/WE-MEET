a40bb6f3daab78ca3e83a2fb6f0cc83024ac8c3a
hwigun46 <hy010827@naver.com>
2025-05-06 19:25:45 +0000

    [FIX] tcp 연결 bpf 수정

--- src/bpf/tcp_connect.bpf.c
+++ src/bpf/tcp_connect.bpf.c
@@ -1,6 +1,8 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_endian.h>
 #include "common.h"
 
 // 라이센스 설정
@@ -15,9 +17,14 @@ struct
     __uint(max_entries, 1024);
 } sock_info_map SEC(".maps");
 
+// map 정의
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} tcp_connect_event_map SEC(".maps");
+
 // 요청이 들어왔을 때 당시 해당 pt_regs에서 소켓 정보 가져와서 map 저장해두기
 SEC("kprobe/tcp_connect")
-int handle_tcp_connect_kprobe(struct pt_regs *ctx)
+static int handle_tcp_connect_kprobe(struct pt_regs *ctx)
 {
     struct sock *sk = (struct sock *)PT_REGS_PARM1(ctx);
 
@@ -28,12 +35,13 @@ int handle_tcp_connect_kprobe(struct pt_regs *ctx)
 
     struct sock_info_t info = {};
 
+    // 더블언더바(__)는 내부 전용 혹은 특별한 용도라는 의미 / 내부에서만 사용하고 외부에서 직접 쓰면 X
     bpf_core_read(&info.saddr, sizeof(info.saddr), &sk->__sk_common.skc_rcv_saddr);
     bpf_core_read(&info.daddr, sizeof(info.daddr), &sk->__sk_common.skc_daddr);
     bpf_core_read(&info.sport, sizeof(info.sport), &sk->__sk_common.skc_num);
     bpf_core_read(&info.dport, sizeof(info.dport), &sk->__sk_common.skc_dport);
 
-    bpf_map_updates_elem(&sock_info_map, &pid, &info, BPF_ANY);
+    bpf_map_update_elem(&sock_info_map, &pid, &info, BPF_ANY);
 
     return 0;
 }
@@ -41,10 +49,12 @@ int handle_tcp_connect_kprobe(struct pt_regs *ctx)
 // map에 저장해둔 소켓 정보 + 연결 결과값 합치기
 // 동적 trcepoint kretporbe로 걸어두기
 SEC("kretprobe/tcp_connect")
-int handle_tcp_connect_kretprobe(struct pt_regs *ctx)
+static int handle_tcp_connect_kretprobe(struct pt_regs *ctx)
 {
+    // tcp 연결 성공 여부
     int ret = (int)PT_REGS_RC(ctx);
 
+    // 연결 실패시 로그 X
     if (ret != 0)
         return 0;
 
@@ -74,7 +84,6 @@ int handle_tcp_connect_kretprobe(struct pt_regs *ctx)
     evt.base.gid = uid_gid & 0xFFFFFFFF;
 
     bpf_get_current_comm(&evt.base.comm, sizeof(evt.base.comm));
-    bpf_core_read_str(&evt.base.parent_comm, sizeof(evt.base.parent_comm), parent->comm);
 
     evt.base.timestamp_ns = bpf_ktime_get_ns();
 
@@ -84,7 +93,7 @@ int handle_tcp_connect_kretprobe(struct pt_regs *ctx)
     evt.dport = bpf_ntohs(info->dport);
     evt.protocol = IPPROTO_TCP;
 
-    bpf_perf_event_output(ctx, &event_output_map, BPF_F_CURRENT_CPU, &evt, sizeof(evt));
+    bpf_perf_event_output(ctx, &tcp_connect_event_map, BPF_F_CURRENT_CPU, &evt, sizeof(evt));
 
     bpf_map_delete_elem(&sock_info_map, &pid);
 
