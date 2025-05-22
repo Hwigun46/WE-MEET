0355acb3c2ab28d9925b6cb4b81b573987e8e0c4
hwigun46 <hy010827@naver.com>
2025-05-22 18:38:49 +0000

    chore: change to ringbuf

--- src/bpf/process_terminate.bpf.c
+++ src/bpf/process_terminate.bpf.c
@@ -10,7 +10,8 @@ char LICENSE[] SEC("license") = "GPL";
 // 맵 타입 설정
 struct
 {
-    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1 << 24);
 } process_terminate_event_map SEC(".maps");
 
 // 프로세스 종료 Tracepoint
@@ -20,46 +21,43 @@ SEC("kprobe/do_exit")
 static int handle_process_terminate(struct pt_regs *ctx)
 {
 
-    struct process_terminate_event_t evt = {};
+    struct process_terminate_event_t *evt;
+    evt = bpf_ringbuf_reserve(&process_terminate_event_map, sizeof(*evt), 0);
+    if (!evt)
+        return 0;
 
     // event type
-    evt.base.event_type = EVENT_PROCESS_TERMINATE;
+    evt->base.event_type = EVENT_PROCESS_TERMINATE;
 
     // pid & tid
     u64 pid_tgid = bpf_get_current_pid_tgid();
 
-    evt.base.pid = pid_tgid >> 32;
-    evt.base.tid = pid_tgid & 0xFFFFFFFF;
+    evt->base.pid = pid_tgid >> 32;
+    evt->base.tid = pid_tgid & 0xFFFFFFFF;
 
     // ppid
     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
     struct task_struct *parent;
 
     bpf_probe_read_kernel(&parent, sizeof(parent), &task->real_parent);
-    bpf_probe_read_kernel(&evt.base.ppid, sizeof(evt.base.ppid), &parent->tgid);
+    bpf_probe_read_kernel(&evt->base.ppid, sizeof(evt->base.ppid), &parent->tgid);
 
     // uid & gid
     u64 uid_gid = bpf_get_current_uid_gid();
 
-    evt.base.uid = uid_gid >> 32;
-    evt.base.gid = uid_gid & 0xFFFFFFFF;
+    evt->base.uid = uid_gid >> 32;
+    evt->base.gid = uid_gid & 0xFFFFFFFF;
 
     // comm
-    bpf_get_current_comm(&evt.base.comm, sizeof(evt.base.comm));
+    bpf_get_current_comm(&evt->base.comm, sizeof(evt->base.comm));
 
     // timestamp
-    evt.base.timestamp_ns = bpf_ktime_get_ns();
+    evt->base.timestamp_ns = bpf_ktime_get_ns();
 
     // exit_code
-    evt.exit_code = PT_REGS_PARM1(ctx);
-
-    // duration
-    u64 start_ns = 0;
-    bpf_probe_read_kernel(&start_ns, sizeof(start_ns), &task->start_time);
-
-    evt.duration_ns = evt.base.timestamp_ns - start_ns;
+    evt->exit_code = PT_REGS_PARM1(ctx);
 
     // event map에 보내기
-    bpf_perf_event_output(ctx, &process_terminate_event_map, BPF_F_CURRENT_CPU, &evt, sizeof(evt));
+    bpf_ringbuf_submit(evt, 0);
     return 0;
 }
